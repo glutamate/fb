014c04860d3c63309babf4efbec5dc1953b786c3
Alex Gremm <alex@beautifuldestinations.com>
2016-05-17 13:40:12 +0100

    + testAdCreation takes all parameters now instead of trying to figure some of them out
    + getAdAccs: returns all AdAccounts

--- src/Facebook/Object/Marketing/Tests.hs
+++ src/Facebook/Object/Marketing/Tests.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables, BangPatterns #-}
 
-module Facebook.Object.Marketing.Tests (testAdCreation) where
+module Facebook.Object.Marketing.Tests (testAdCreation, getAdAccs) where
 import System.Environment
 import Network.HTTP.Conduit
 import qualified Data.Text as T
@@ -35,22 +35,31 @@ import qualified Data.Map.Strict as Map
 import Data.Aeson
 import Data.Either
 import qualified Prelude as P
+import Data.Monoid
+import Control.Monad.IO.Class
 
-testAdCreation fbUserId fbToken fbPageId igTargetId creds = do
-  fbUrl <- liftM T.pack $ getEnv "FB_URL"
+getAdAccs fbUserId fbToken creds = do
   man <- newManager conduitManagerSettings
   now <- getCurrentTime
-  let --creds = Credentials "bdpromo" (T.pack appId) (T.pack appSecret)
-      fbuser = FB.Id (T.pack $ show fbUserId)
+  let fbuser = FB.Id (T.pack $ show fbUserId)
       tokExpires = addUTCTime 1000 now
       tok = UserAccessToken fbuser fbToken now
   runResourceT $ runFacebookT creds man $ do
     Pager adaccids _ _ <- getAdAccountId $ Just tok
-    Pager (igId':_) _ _ <- getIgId tok fbPageId
-    let igId = unId_ $ id igId'
+    mapM (\acc -> getAdAccount (id acc) (Name ::: Nil) $ Just tok) adaccids
+
+testAdCreation fbAccId igId fbUserId fbToken fbPageId igTargetId creds = do
+  let fbId = Id_ $ T.pack fbAccId
+  let fbUrl = "https://www.facebook.com/BeautifulDestinations"
+  man <- newManager conduitManagerSettings
+  now <- getCurrentTime
+  let fbuser = FB.Id (T.pack $ show fbUserId)
+      tokExpires = addUTCTime 1000 now
+      tok = UserAccessToken fbuser fbToken now
+  runResourceT $ runFacebookT creds man $ do
     let campaign = (Name, Name_ "Test Campaign API ad creation") :*: (Objective, Objective_ OBJ_LINK_CLICKS)
                    :*: (AdC.Status, AdC.Status_ PAUSED_) :*: (BuyingType, BuyingType_ AUCTION) :*: Nil
-    ret' <- setAdCampaign (id $ head adaccids) campaign tok
+    ret' <- setAdCampaign fbId campaign tok
     let ret = either (error . show) P.id ret'
     let campaign = ret
     let location = TargetLocation ["US", "GB"]
@@ -60,9 +69,9 @@ testAdCreation fbUserId fbToken fbPageId igTargetId creds = do
                 :*: (CampaignId, CampaignId_ $ campaignId ret) :*: (Targeting, Targeting_ target)
                 :*: (OptimizationGoal, OptimizationGoal_ REACH)
                 :*: (BillingEvent, BillingEvent_ IMPRESSIONS_) :*: (DailyBudget, DailyBudget_ 500) :*: Nil
-    adsetRet' <- setAdSet (id $ head adaccids) adset tok
+    adsetRet' <- setAdSet fbId adset tok
     let adsetRet = either (error . show) P.id adsetRet'
-    Pager images _ _ <- getAdImage (id $ head adaccids) Nil tok
+    Pager images _ _ <- getAdImage fbId Nil tok
     let imgHash = FBT.hash $ head images
     let cta_value = CallToActionValue fbUrl "FIXME"
     let call_to_action = Just $ CallToActionADT LEARN_MORE cta_value
@@ -72,11 +81,11 @@ testAdCreation fbUserId fbToken fbPageId igTargetId creds = do
     let oss = ObjectStorySpecADT link fbPageId $ Just $ IgId igId
     let adcreative = (Name, Name_ "Test AdCreative")
                     :*: (ObjectStorySpec, ObjectStorySpec_ oss) :*: Nil
-    creativeRet' <- setAdCreative (id $ head adaccids) adcreative tok
+    creativeRet' <- setAdCreative fbId adcreative tok
     let !creativeRet = either (error . show) P.id creativeRet'
     let ad = (Creative, Creative_ $ creativeToCreative creativeRet) :*: (AdsetId, AdsetId_ $ adsetIdToInt adsetRet)
             :*: (Name, Name_ "Another Test Ad! API") :*: (Ad.Status, Ad.Status_ PAUSED_) :*: Nil
-    adId' <- setAd (id $ head adaccids) ad tok
+    adId' <- setAd fbId ad tok
 
     updAdCampaign campaign ((AdC.Status, AdC.Status_ DELETED_) :*: Nil) tok
-    return (igId, unId_ $ id $ head adaccids)
+    return ()
