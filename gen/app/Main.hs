38a19934e95e52553fe0eb3284bace26806e16db
Alex Gremm <alex@beautifuldestinations.com>
2016-06-27 14:59:46 +0100

    minor

--- gen/app/Main.hs
+++ gen/app/Main.hs
@@ -21,17 +21,15 @@ csvFiles = V.fromList ["data/adaccount.csv", "data/adcampaign.csv", "data/adset.
 
 main :: IO ()
 main = do
-    inps <- V.mapM BS.readFile csvFiles
-    let csvs = V.map (decode HasHeader) inps :: V.Vector (Either String (V.Vector CsvLine))
-    let l = V.toList csvs
-    assert (V.length csvs == Prelude.length l) $ return ()
-    putStrLn "Generating source code ..."
-    let env = buildEnv $ V.fromList $ rights l
-    saveFiles $ genFiles env
+  inps <- V.mapM BS.readFile csvFiles
+  let csvs = V.map (decode HasHeader) inps :: V.Vector (Either String (V.Vector CsvLine))
+  let l = V.toList csvs
+  assert (V.length csvs == Prelude.length l) $ return ()
+  putStrLn "Generating source code ..."
+  let env = buildEnv $ V.fromList $ rights l
+  V.mapM_ saveFile $ genFiles env
 
-saveFiles :: V.Vector (FilePath, Text) -> IO ()
-saveFiles = V.mapM_ save
-    where
-        save (path, data_) = do
-            putStrLn $ "Saving " ++ path
-            T.writeFile ("../src/" <> path) data_
+saveFile :: (FilePath, Text) -> IO ()
+saveFile (path, data_) = do
+  putStrLn $ "Saving " ++ path
+  T.writeFile ("../src/" <> path) data_
