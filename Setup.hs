b59bc3614b270bbce3b68f091dd596eacd7dd544
Ben Clifford <ben@beautifuldestinations.com>
2016-10-26 08:42:19 +0000

    Cabal flag to build from CSVs
    Previously, it was necessary to manually go
    into gen/ and stack build; stack exec the
    code there, every time you changed any
    of the definitions there.
    
    This flag makes that happen as part of the
    top level build. For example:
    
    stack build --flag fb:regenerate

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,35 @@
+{-# Language CPP #-}
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Distribution.PackageDescription (emptyHookedBuildInfo, FlagName (..))
+import System.Process
+import System.Environment (getArgs)
+import System.IO (hFlush, hPutStrLn, stderr)
+
+main = do
+  defaultMainWithHooks hooks
+
+hooks = simpleUserHooks {
+    buildHook = optionallyRegenerate (buildHook simpleUserHooks)
+  }
+
+optionallyRegenerate cont packageDescription localBuildInfo userHooks buildFlags = do
+  let flags = configConfigurationsFlags $ configFlags localBuildInfo
+  let flag = lookup (FlagName "regenerate") flags
+  case flag of
+    Just True -> regenerate
+    _ -> progress "Not regenerating"
+
+  cont packageDescription localBuildInfo userHooks buildFlags
+  return ()
+  -- return emptyHookedBuildInfo -- TODO: this should return info that causes regeneration to happen approprately?
+
+regenerate = do
+  progress "Regenerating from gen/"
+  callCommand "cd gen && stack build && stack exec gen-exe"
+  progress "Regeneration done"
+
+progress s = do
+  hPutStrLn stderr s
+  hFlush stderr
